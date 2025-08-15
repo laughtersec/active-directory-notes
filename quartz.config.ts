7215da1afe5b0da98d1c182518363851d1056689
laughtersec <178651847+laughtersec@users.noreply.github.com>
2025-08-15 16:16:52 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "laughtersec.github.io",
+    baseUrl: "laughtersec.github.io/active-directory-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
